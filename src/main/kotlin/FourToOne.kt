d289614f306735d55cef327380b8850179745bbb
Christopher Sumnicht <csumnicht@berkeley.edu>
2017-01-01 20:18:18 -0800

    Fixed primitives, "completion" of four to one

--- src/main/kotlin/FourToOne.kt
+++ src/main/kotlin/FourToOne.kt
@@ -4,23 +4,28 @@ class FourToOne : Game<Int, Int> {
     override val initialPos = 4
 
     override fun doMove(p: Int, m: Int): Int {
-        return 0
+        return p - m
 
     }
     
     override fun genMoves(p: Int): Array<Int> {
-        return emptyArray<Int>()
+        when {
+            p == 1 -> return arrayOf(0)
+            else   -> return arrayOf(p - 1, p - 2)
+        }
     }
 
     override fun primitive(p: Int): Primitive {
-        return Primitive.WIN
+        if (p == 0) {
+            return Primitive.LOSS
+        } else {
+            return Primitive.UNDECIDED
+        }
     }
 }
 
-class Main {
+object Main {
     
-
-
     fun main() {
 
     }
--- src/main/kotlin/Primitive.kt
+++ src/main/kotlin/Primitive.kt
@@ -1,5 +1,5 @@
 package solve
 
 enum class Primitive {
-    WIN, LOSS, TIE, DRAW
+    WIN, LOSS, TIE, DRAW, UNDECIDED
 }
