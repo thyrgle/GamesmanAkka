8927d59bfd6572692178044384b3e109331748bb
Christopher Sumnicht <csumnicht@berkeley.edu>
2017-01-26 21:55:32 -0800

    More docstrings.

--- src/main/kotlin/Solver.kt
+++ src/main/kotlin/Solver.kt
@@ -5,14 +5,32 @@ import akka.actor.ActorRef
 import akka.actor.Props
 import akka.actor.UntypedActor
 
+/**
+ * A group of "sovlers" that solve games!
+ *
+ * @param Pos the data type that represents a game position (or state).
+ * @param Move the data type that represents a game move.
+ */
 class Solver<Pos, Move> : UntypedActor() {
 
     var actors: MutableList<ActorRef> = arrayListOf()
-
+    
+    /**
+     * Given a hashed number, get the appropriate actor.
+     * @param hash the hashed integer.
+     * @return the actor associated with the particular hash.
+     */
     fun actorForHash(hash: Int): ActorRef {
         return actors.get(hash % Config.ACTOR_COUNT)
     }
-
+    
+    /**
+     * Run the solver as a whole!
+     * 
+     * Responsible for initializing the actor system and then starting it off!
+     *
+     * @param game the game to be solved.
+     */
     fun run(game: Game<Pos, Move>) {
         val system = ActorSystem.create("SolverSystem")
         for (i in 1..Config.ACTOR_COUNT) {
