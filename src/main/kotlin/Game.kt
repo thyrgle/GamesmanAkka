2085d3f740ed0eafa84d7bb8f9e56703c31dc406
Christopher Sumnicht <csumnicht@berkeley.edu>
2017-01-26 21:59:32 -0800

    Cleaned up game interface

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,14 +1,14 @@
 package solve
 
+/**
+ * Interface describing what a game must conform to for the solver to use.
+ * @param Pos the datatype representing a position.
+ * @param Move the datatype representing a move.
+ */
 interface Game<Pos, Move> {
-
     val initialPos: Pos
-
     fun doMove(p: Pos, m: Move): Move
-
     fun genMoves(p: Pos): Array<Move>
-
     fun primitive(p: Pos): Primitive
-
     fun hashPosition(p: Pos): Int
 }
