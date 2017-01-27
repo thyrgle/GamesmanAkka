70ce1602f362827f4f594d840003358f65629757
Christopher Sumnicht <csumnicht@berkeley.edu>
2017-01-26 22:01:30 -0800

    Added docstrings to Lookup and Resolve

--- src/main/kotlin/Lookup.kt
+++ src/main/kotlin/Lookup.kt
@@ -1,5 +1,10 @@
 package solve
 
+/**
+ * Lookup object.
+ *
+ * No logic in this class. Lookup is meant to be interpreted by onReceive.
+ */
 class Lookup<Pos>(position: Pos) {
     val position = position
 }
--- src/main/kotlin/Resolve.kt
+++ src/main/kotlin/Resolve.kt
@@ -1,5 +1,10 @@
 package solve
 
+/**
+ * Resolve class.
+ *
+ * No logic in this class. Meant to be interpreted by onReceive.
+ */
 class Resolve<Pos>(position: Pos) {
     val position = position
 }
