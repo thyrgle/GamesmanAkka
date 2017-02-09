420db599bb88cf75f25ade5c4baf743a94cb3dea
Henry <hyi@berkeley.edu>
2017-02-08 16:03:27 -0800

    structural changes in Solver fixed hash off by 1

--- src/main/kotlin/Solver.kt
+++ src/main/kotlin/Solver.kt
@@ -6,15 +6,16 @@ import akka.actor.Props
 import akka.actor.UntypedActor
 
 /**
- * A group of "sovlers" that solve games!
+ * A group of "solvers" that solve games!
  *
  * @param Pos the data type that represents a game position (or state).
  * @param Move the data type that represents a game move.
  */
-class Solver<Pos, Move> : UntypedActor() {
+class Solver<Pos, Move> (game: Game<Pos, Move>) {
 
-    var actors: MutableList<ActorRef> = arrayListOf()
-    
+    val actors: MutableList<ActorRef> = arrayListOf()
+    val game: Game<Pos, Move> = game;
+
     /**
      * Given a hashed number, get the appropriate actor.
      * @param hash the hashed integer.
@@ -31,70 +32,76 @@ class Solver<Pos, Move> : UntypedActor() {
      *
      * @param game the game to be solved.
      */
-    fun run(game: Game<Pos, Move>) {
+    fun run() {
         val system = ActorSystem.create("SolverSystem")
-        for (i in 1..Config.ACTOR_COUNT) {
+        for (i in 0..(Config.ACTOR_COUNT - 1)) {
             actors.add(
-                system.actorOf(Props.create(Solver::class.java), "Solver" + i.toString())
+                system.actorOf(Props.create(SolverActor::class.java), "Solver" + i.toString())
             )
         }
         val master = actorForHash(game.hashPosition(game.initialPos))
     }
 
-    val solvedPositions: MutableMap<Pos, Primitive> =  mutableMapOf()
+    inner class SolverActor: UntypedActor() {
 
-     /**
-      * Determine if a particular position has been solved. If not, return null.
-      *
-      * @param position the position to be looked up.
-      * @return the primitive of the value of the position if it exists.
-      */
-     fun lookup(position: Pos): Primitive? {
-        if (solvedPositions.containsKey(position)) {
-            return solvedPositions[position]
-        } else {
-            return null
-        }
-     }
+        val solvedPositions: MutableMap<Pos, Primitive> =  mutableMapOf()
 
-     /**
-      * Given a particular position, resolve the position if it can be resolved.
-      * If it can't update the counter to indicate that it only needs so many more
-      * iterations until it can be.
-      *
-      * @param position to be resolved.
-      * @return success (or failure) of resolving the position.
-      */
-     fun resolve(position: Pos): Boolean {
-         // TODO.
-         return false
-     }
-    
-     /**
-      * Generate a positions children and distribute them to everyone.
-      *
-      * @param Pos he position to be distributed.
-      */
-     fun distribute(position: Pos) {
-     }
-
-    override fun onReceive(msg: Any?) {
-        when (msg) {
-            is Lookup<*> -> {
-                val result = lookup(msg.position as Pos)
-                if (result != null) {
-                    // TODO: Send back to appropriate actor.
-                } else {
-                    distribute(msg.position as Pos)
-                }
+        /**
+         * Determine if a particular position has been solved. If not, return null.
+         *
+         * @param position the position to be looked up.
+         * @return the primitive of the value of the position if it exists.
+         */
+        fun lookup(position: Pos): Primitive? {
+            if (solvedPositions.containsKey(position)) {
+                return solvedPositions[position]
+            } else {
+                return null
             }
-            is Resolve<*> ->  {
-                if (resolve(msg.position as Pos)) {
-                    // The position was successfully resolved.
-                    // TODO: Send back to appropriate actor.
-                } else {
-                    // Still needs to wait for stuff to finish.
-                    // TODO?
+        }
+
+        /**
+         * Given a particular position, resolve the position if it can be resolved.
+         * If it can't update the counter to indicate that it only needs so many more
+         * iterations until it can be.
+         *
+         * @param position to be resolved.
+         * @return success (or failure) of resolving the position.
+         */
+        fun resolve(position: Pos): Boolean {
+            // TODO.
+
+            return false
+        }
+
+        /**
+         * Generate a positions children and distribute them to everyone.
+         *
+         * @param Pos he position to be distributed.
+         */
+        fun distribute(position: Pos) {
+        }
+
+        override fun onReceive(msg: Any?) {
+            when (msg) {
+                is Lookup<*> -> {
+                    val result = lookup(msg.position as Pos)
+
+                    if (result != null) {
+                        // TODO: Send back to appropriate actor.
+                        val toSend = result
+                    } else {
+                        distribute(msg.position)
+                    }
+                }
+                is Resolve<*> ->  {
+                    if (resolve(msg.position as Pos)) {
+                        // The position was successfully resolved.
+                        // TODO: Send back to appropriate actor.
+                    } else {
+                        // Still needs to wait for stuff to finish.
+                        // TODO?
+                    }
                 }
             }
         }
